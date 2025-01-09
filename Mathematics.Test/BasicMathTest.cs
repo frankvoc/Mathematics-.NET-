c1e4c9ef0fb4d65eb9e6e52c86dbb2d804ccdc44
008020246 <008020246@N215C6-24>
2025-01-09 18:15:35 -0500

    Add project files.

---
+++ Mathematics.Test/BasicMathTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mathematics.Test
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNum()
+        {
+            var math = new BasicMath();
+            var result = math.AddNumbers(2, 3);
+            Assert.True(result == 5);
+        }
+    }
+}
---
+++ Mathematics/BasicMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class BasicMath
+    {
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double SubNumbers(double num1, double num2)
+
+            {
+                return num1 - num2;
+            }
+        public double MultNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double DivNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
